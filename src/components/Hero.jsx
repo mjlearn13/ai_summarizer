b110a6d147b99bbd2caf5e26da41e7491116633a
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2023-09-07 12:11:21 +0100

    completed project

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
 
         <button
           type="button"
-          onClick={() => window.open('https://github.com/mjlearn13')}
+          onClick={() => window.open('https://github.com/mjlearn13', "_blank")}
           className="black_btn"
         >
           GitHub
@@ -16,7 +16,8 @@ const Hero = () => {
       </nav>
 
       <h1 className="head_text">
-        Summarize Articles with <br className="max-md:hidden" /> <span className="orange_gradient">OpenAI GPT-4</span>
+        Summarize Articles with <br className="max-md:hidden" /> 
+        <span className="orange_gradient">OpenAI GPT-4</span>
       </h1>
       <h2 className="desc">
         Simplify your reading with Summize, an open-source article summarizer
